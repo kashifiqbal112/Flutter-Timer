568c30c2dae07d1eab8855341f2a4eae51aad9c7
kashif <kiq@embrace-it.com>
2024-04-02 22:21:47 +0500

    Foreground service added

--- android/app/src/main/kotlin/com/example/timer/service/TimerService.kt
+++ android/app/src/main/kotlin/com/example/timer/service/TimerService.kt
@@ -1,4 +1,74 @@
 package com.example.timer.service
 
-class TimerService {
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.os.IBinder
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
+import com.example.timer.MainActivity
+
+
+class TimerService  : Service() {
+
+    public override fun onStartCommand(
+        intent: Intent?,
+        flags: kotlin.Int,
+        startId: kotlin.Int
+    ): kotlin.Int {
+        createNotificationChannel()
+        val notification =
+            buildForegroundNotification("Timer Service", "Timer is running in background.. ")
+        startForeground(123, notification)
+        return START_STICKY
+    }
+    override fun onBind(p0: Intent?): IBinder? {
+        return null
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name: CharSequence = "timer"
+            val description ="this is timer notification"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel("111", name, importance)
+            channel.description = description
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            val notificationManager = getSystemService(
+                NotificationManager::class.java
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    private fun buildForegroundNotification(title: String, text: String): Notification {
+        val notificationIntent = Intent(this, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(
+            this, 0,
+            notificationIntent, PendingIntent.FLAG_IMMUTABLE
+        )
+        return NotificationCompat.Builder(this, "111")
+            .setContentTitle(title)
+            .setContentText(text)
+            .setSmallIcon(com.example.timer.R.mipmap.ic_launcher)
+            .setContentIntent(pendingIntent)
+            .setTicker(text)
+            .build()
+    }
+
+
+    companion object {
+        const val TAG = "TimerForegroundService"
+
+        fun start(context: Context) {
+            val intent = Intent(context, TimerService::class.java)
+            ContextCompat.startForegroundService(context, intent)
+        }
+    }
 }
---
+++ lib/helper/native_service_helper.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/services.dart';
+
+class NativeServiceHelper {
+  static const MethodChannel _channel = MethodChannel('com.example.timer/Timer');
+
+  static Future<void> startForegroundService() async {
+    try {
+      await _channel.invokeMethod('startForegroundService');
+    } on PlatformException catch (e) {
+      print("Failed to start the service: '${e.message}'.");
+    }
+  }
+}
